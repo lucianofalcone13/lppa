409839e6f4e6d981f577451ce93090914b04a5f8
Luciano Falcone <lucianoafalcone@gmail.com>
2020-12-09 11:04:17 -0300

    validations and submit handlers

--- js/index.js
+++ js/index.js
@@ -1,82 +1,85 @@
 var button = null;
 var inputs = {};
-var errors = {};
+var errors = {
+  email: '',
+  password: '',
+};
+var data = {
+  email: '',
+  password: '',
+};
 
 function handleSubmit(evt) {
-    evt.preventDefault();
-    console.log('here');
+  evt.preventDefault();
+  alert('Data has been sent \n' + 'Email: ' + data.email + '\n' + 'Password: ' + data.password + '\n');
 }
 
 function handleFocus(evt) {
-    clearError(evt.target.name);
+  clearError(evt);
 }
 
-function clearError(name) {
-    if (errors[name]) {
-        delete errors[name];
-    }
+function clearError(evt) {
+  data[evt.target.name] = evt.target.value;
+  if (errors[evt.target.name]) {
+    evt.target.classList.remove('error');
+    evt.target.nextElementSibling.innerHTML = '';
+    delete errors[evt.target.name];
+  }
+}
+
+function displayError(evt, errorMsg) {
+  evt.target.classList.add('error');
+  errors[evt.target.name] = errorMsg;
+  evt.target.nextElementSibling.innerHTML = errors[evt.target.name];
 }
 
 function handleBlur(evt) {
-    var target = evt.target;
-    if (target.name === 'email') {
-        var emailIsValid = validateEmail(target.value);
-        if (emailIsValid) {
-            target.classList.remove('error');
-            evt.target.nextElementSibling.innerHTML = '';
-            delete errors.email;
-        } else {
-            evt.target.classList.add('error');
-            errors.email = 'Invalid Email';
-            evt.target.nextElementSibling.innerHTML = errors.email;
-        }
+  var target = evt.target;
+  if (target.name === 'email') {
+    var emailIsValid = validateEmail(target.value);
+    if (emailIsValid) {
+      clearError(evt);
     } else {
-        var passwordIsValid = validatePassword(target.value);
-        if (passwordIsValid) {
-            target.classList.remove('error');
-            delete errors.password;
-            evt.target.nextElementSibling.innerHTML = '';
-        } else {
-            evt.target.classList.add('error');
-            errors.password = 'Invalid Password';
-            evt.target.nextElementSibling.innerHTML = errors.password;
-        }
+      displayError(evt, 'Invalid Email');
     }
-    handleSubmitButtonState();
+  } else {
+    var passwordIsValid = validatePassword(target.value);
+    if (passwordIsValid) {
+      clearError(evt);
+    } else {
+      displayError(evt, 'Invalid Password');
+    }
+  }
+  handleSubmitButtonState();
 }
 
 function handleSubmitButtonState() {
-    console.log(errors);
-    if (!Object.values(errors).length) {
-        button.disabled = false;
-    } else {
-        button.disabled = true;
-    }
+  button.disabled = !!errors.email || !!errors.password;
 }
 
 function validatePassword(value) {
-    return !!value;
+  return !!value;
 }
 
 function validateEmail(value) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(value).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(value).toLowerCase());
 }
 
 function getFormElements() {
-    inputs = document.getElementsByClassName('input');
-    button = document.getElementById('login-button');
+  inputs = document.getElementsByClassName('input');
+  button = document.getElementById('login-button');
 }
 
 function attachHandlers() {
-    inputs.email.onblur = handleBlur;
-    inputs.password.onblur = handleBlur;
-    inputs.email.onfocus = handleFocus;
-    inputs.password.onfocus = handleFocus;
-    button.onclick = handleSubmit;
+  inputs.email.onblur = handleBlur;
+  inputs.password.onblur = handleBlur;
+  inputs.email.onfocus = handleFocus;
+  inputs.password.onfocus = handleFocus;
+  button.onclick = handleSubmit;
 }
 
 window.onload = function () {
-    getFormElements();
-    attachHandlers();
+  getFormElements();
+  attachHandlers();
 }
